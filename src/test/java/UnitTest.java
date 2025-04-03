6536516fac6c0ee8ee188e2a3f37674e3ddc4b39
SStychev <SStychev@srt-llc.ru>
2025-04-03 21:36:45 +0400

    CI again

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UnitTest {
+
+    @Test
+    public void testCloseOneWindow() {
+        assertEquals(1,1);
+    }
+
+}
